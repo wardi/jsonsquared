63a73838432a45a24e541b48c21a6a6a8642fd58
Ian Ward <ian@excess.org>
2014-09-21 10:37:04 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+install_requires=[
+    'setuptools',
+    'docopt',
+    'xlrd',
+    'xlwt',
+]
+if sys.version_info <= (3,):
+    install_requires.append('simplejson')
+
+setup(
+    name='json-squared',
+    version='0.1',
+    description=
+        'Convert JSON to easily editable CSV/XLS'
+    license='MIT',
+    author='Ian Ward',
+    author_email='ian@excess.org',
+    url='https://github.com/wardi/json-squared',
+    packages=[
+        'json2',
+        ],
+    install_requires=install_requires,
+    test_suite='json2.tests',
+    zip_safe=False,
+    entry_points = """
+        [console_scripts]
+        json2=json2.cli:main
+        """
+    )
