875105e00e580681934610bd9ad65f55f81daeae
Ian Ward <ian@excess.org>
2014-10-05 11:47:07 -0400

    start of csv string conversions

---
+++ jsonsquared/csv_string.py
@@ -0,0 +1,23 @@
+# Convert between CSV strings and python-equivalent JSON types
+
+def decode(s):
+    """
+    Return the python-equivalent object for a JSON values stored
+    as a string in a CSV or spreadsheet.
+
+    :param s: unicode CSV string
+    :param list_delimiter: None or a unicode string used to separate
+        values stored as a list within a single cell
+
+    :returns
+    """
+
+def decode_list(s, list_delimiter):
+    """
+    Return the python-equivalent list of objects for a JSON values
+    stored as a string in a CSV or spreadsheet.
+
+    :param s: unicode CSV string
+    :param list_delimiter: unicode string used to separate
+        values stored as a list within this column
+    """
